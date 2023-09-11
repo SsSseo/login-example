9649423f485ac06ea56656a69a530eecf969a910
seohyeon <psh970709@gmail.com>
2023-09-12 00:54:59 +0900

    Head 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Toaster from "./toast";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body className={inter.className}>
         <Toaster position="top-center" />
         {children}
